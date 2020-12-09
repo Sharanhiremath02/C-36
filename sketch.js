b08a6324df12961ff65838cd6d3d1815a63af3e4
Sharanhiremath02 <72602017+Sharanhiremath02@users.noreply.github.com>
2020-12-09 16:59:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var  database,form,player,game,playerCount;
+var gameState=0;
+
+
+function setup(){
+  database = firebase.database();
+ // console.log(database);
+  createCanvas(400,400);
+ game=new Game();
+ game.getState();
+ game.start();
+ 
+  
+}
+
+function draw(){
+ 
+  
+  
+}
+
